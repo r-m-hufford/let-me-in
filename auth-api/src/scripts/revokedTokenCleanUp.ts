1c718da8d0e16a8c44ec230f7fcf3a5b52f8ffb6
Ryan Hufford <ryan@rmhufford.com>
2023-08-05 10:29:58 -0400

    database connection in clean up script

--- auth-api/src/scripts/revokedTokenCleanUp.ts
+++ auth-api/src/scripts/revokedTokenCleanUp.ts
@@ -1,5 +1,27 @@
-async function initialize() {
-  console.log('we gotta function here!');
-}
+import "reflect-metadata";
+import { myDataSource } from "../../app-data-source";
+import { RevokedToken } from "../models/revoked-token";
+const revokedTokenRepo = myDataSource.getRepository(RevokedToken);
 
-initialize();
+import { User } from "../models/user";
+const userRepo = myDataSource.getRepository(User);
+
+
+(async function() {
+  console.log('begin revoked token table clean up...');
+  
+  await myDataSource
+  .initialize()
+  .then(() => {
+    console.log('data source initialized in token clean up funciton');
+  })
+  .catch((err) => {
+    console.error('error initializing data source:', err);
+  });
+
+  console.log(myDataSource.isInitialized);
+  const revoked = await revokedTokenRepo.find();
+
+  console.log({ revoked })
+})();
+
