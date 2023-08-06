04cedfc39d7b3a7f8c8c8dfb5a3908dbff4f8332
Ryan M Hufford <ryan@rmhufford.com>
2023-08-05 21:18:48 -0400

    revoked tokens clean up script (#19)
    * initial commit
    
    * ignore node mods at root of project
    
    * database connection in clean up script
    
    * connection in script
    
    * completed clean up script
    
    * remove log. add script to package json

---
+++ auth-api/src/scripts/revokedTokenCleanUp.ts
@@ -0,0 +1,41 @@
+import "reflect-metadata";
+import { myDataSource } from "../../app-data-source";
+import { RevokedToken } from "../models/revoked-token";
+const revokedTokenRepo = myDataSource.getRepository(RevokedToken);
+
+import { User } from "../models/user";
+const userRepo = myDataSource.getRepository(User);
+
+
+(async function() {
+  console.log('begin revoked token table clean up...');
+  
+  await myDataSource
+  .initialize()
+  .then(() => {
+    console.log('data source initialized in token clean up funciton');
+  })
+  .catch((err) => {
+    console.error('error initializing data source:', err);
+  });
+
+  const revokedTokens = await revokedTokenRepo.find();
+
+  for ( let revokedToken of revokedTokens ) {
+    if (revokedToken.expiresAt < new Date) {
+      console.log(revokedToken.expiresAt);
+      await revokedTokenRepo.delete(revokedToken.revokedTokenId);
+    }
+  }
+
+  await myDataSource
+  .destroy()
+  .then(() => {
+    console.log('database connection closed')
+  })
+  .catch((err => {
+    console.log('error closing database connection')
+  }));
+  console.log('finialize revoked token table clean up');
+})();
+
