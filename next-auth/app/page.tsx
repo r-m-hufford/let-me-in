3e17a461204a066f616db45295aaf71448c3e2e3
Ryan Hufford <ryan@rmhufford.com>
2023-09-24 18:12:38 -0400

    tailwind tinkering and planning

--- next-auth/app/page.tsx
+++ next-auth/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>let-me-in</h1>
+      <h1 className='text-amber-500'>let-me-in</h1>
     </main>
   )
 }
